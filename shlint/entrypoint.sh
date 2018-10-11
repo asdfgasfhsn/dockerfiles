9dd63d29f25fcbadb0e80bdafe531314ef3ff715
asdfgasfhsn <dude@asdfgasfhsn.xyz>
2018-10-11 15:26:18 +1100

    wip: first attempt at argument pass through for shlint

--- shlint/entrypoint.sh
+++ shlint/entrypoint.sh
@@ -2,15 +2,23 @@
 export TERM=xterm-256color
 
 case $1 in
-  shellcheck)
-    shift
-    /bin/shellcheck "/home/shlint/lint/${*}"
+shellcheck)
+  shift
+  /bin/shellcheck "/home/shlint/lint/${*}"
   ;;
-  shfmt)
-    shift
-    /bin/shfmt "/home/shlint/lint/${*}"
+shfmt)
+  shift
+  args=""
+  filename=""
+  length="$(($# - 1))"
+  if [[ "${length}" -gt 1 ]]; then
+    args="${*:1:$length}"
+  fi
+  filename="${*: -1}"
+  echo "/bin/shfmt ${args} /home/shlint/lint/${filename}"
+  /bin/shfmt ${args} "/home/shlint/lint/${filename}"
   ;;
-  *)
-    "$@"
+*)
+  "$@"
   ;;
 esac
