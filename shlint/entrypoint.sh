746ecaf2f51bebe9cb7d2f6e60ef7c3334caece5
asdfgasfhsn <dude@asdfgasfhsn.xyz>
2018-10-11 15:35:47 +1100

    fix indent

--- shlint/entrypoint.sh
+++ shlint/entrypoint.sh
@@ -2,29 +2,29 @@
 export TERM=xterm-256color
 
 case $1 in
-shellcheck)
-  shift
-  args=""
-  filename=""
-  length="$(($# - 1))"
-  if [[ "${length}" -gt 1 ]]; then
-    args="${*:1:$length}"
-  fi
-  filename="${*: -1}"
-  /bin/shellcheck "/home/shlint/lint/${*}"
-  ;;
-shfmt)
-  shift
-  args=""
-  filename=""
-  length="$(($# - 1))"
-  if [[ "${length}" -gt 1 ]]; then
-    args="${*:1:$length}"
-  fi
-  filename="${*: -1}"
-  /bin/shfmt ${args} "/home/shlint/lint/${filename}"
-  ;;
-*)
-  "$@"
-  ;;
+  shellcheck)
+    shift
+    args=""
+    filename=""
+    length="$(($# - 1))"
+    if [[ "${length}" -gt 1 ]]; then
+      args="${*:1:$length}"
+    fi
+    filename="${*: -1}"
+    /bin/shellcheck "/home/shlint/lint/${*}"
+    ;;
+  shfmt)
+    shift
+    args=""
+    filename=""
+    length="$(($# - 1))"
+    if [[ "${length}" -gt 1 ]]; then
+      args="${*:1:$length}"
+    fi
+    filename="${*: -1}"
+    /bin/shfmt ${args} "/home/shlint/lint/${filename}"
+    ;;
+  *)
+    "$@"
+    ;;
 esac
