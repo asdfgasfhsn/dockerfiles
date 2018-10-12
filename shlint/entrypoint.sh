01ff9001d280fdd0c8ae1bca0aea73faaa211643
asdfgasfhsn <dude@asdfgasfhsn.xyz>
2018-10-12 16:40:33 +1100

    shlint: update dockerfile & entrypoint

--- shlint/entrypoint.sh
+++ shlint/entrypoint.sh
@@ -1,40 +1,24 @@
 #!/bin/bash
 export TERM=xterm-256color
-
 declare -r LINT_PATH='/home/shlint/lint/'
-shfmt_args="${shfmt_args:--d -i 2 -ci}"
-
+export SHFMT_OPTS="${SHFMT_OPTS:--w -d -i 2 -ci}"
+export SHELLCHECK_OPTS="${SHELLCHECK_OPTS:-}"
+export filename="${*: -1}"
 case $1 in
   shellcheck)
-    shift
-    args=""
-    filename=""
-    length="$(($# - 1))"
-    if [[ "${length}" -gt 1 ]]; then
-      args="${*:1:$length}"
-    fi
-    filename="${*: -1}"
-    /bin/shellcheck "${args}" "${LINT_PATH}${filename}"
+    /bin/shellcheck "${LINT_PATH}${filename}"
     ;;
   shfmt)
-    shift
-    args=""
-    filename=""
-    length="$(($# - 1))"
-    if [[ "${length}" -gt 1 ]]; then
-      args="${*:1:$length}"
-    fi
-    filename="${*: -1}"
-    /bin/shfmt "${args}" "${LINT_PATH}${filename}"
+    # shellcheck disable=SC2086
+    /bin/shfmt ${SHFMT_OPTS} "${LINT_PATH}${filename}"
     ;;
   lint)
-    filename="${*: -1}"
     echo "Parsing ${filename} with shellcheck..."
     /bin/shellcheck "${LINT_PATH}${filename}"
     echo "Parsing ${filename} with shfmt..."
     # shellcheck disable=SC2086
-    /bin/shfmt ${shfmt_args} "${LINT_PATH}${filename}"
-  ;;
+    /bin/shfmt ${SHFMT_OPTS} "${LINT_PATH}${filename}"
+    ;;
   *)
     "$@"
     ;;
