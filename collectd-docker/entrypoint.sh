45949ccfb5b4829e48bf6ee583f4b502a2fd80a1
asdfgasfhsn <dude@asdfgasfhsn.xyz>
2019-03-14 11:43:18 +1100

    [collectd-docker] tweaks to entrypoint & dockerfile, adjustments to collectd.conf and compose template resource allocation

--- collectd-docker/entrypoint.sh
+++ collectd-docker/entrypoint.sh
@@ -3,13 +3,15 @@
 set -e
 
 if [ -d /mnt/proc ]; then
+  echo "[INFO] /mnt/proc detected, remounting as /proc to expose host process info"
   umount /proc
   mount -o bind /mnt/proc /proc
 fi
 
 if [ -z "$@" ]; then
-  exec /usr/sbin/collectd -C /etc/collectd/collectd.conf -f
+  cmd='/usr/sbin/collectd -C /etc/collectd/collectd.conf -f'
+  echo "[INFO] exec \"${cmd}\""
+  exec ${cmd}
 else
   exec $@
 fi
-
